82660e0bda7ce1d06e431a37adc04729e24a6c93
Gang Tao <gtao@splunk.com>
2019-02-17 12:29:53 -0800

    using openshift node port/ip

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@
 import http from 'http'
 import app from './app'
 
+const server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 //create node.js http server and listen on port
-const server = http.createServer(app()).listen(3000, () => {
+const server = http.createServer(app()).listen(server_port, server_ip_address, () => {
     const host = server.address().address;
     const port = server.address().port;
     console.log('umi mock serve at http://%s:%s', host, port);
